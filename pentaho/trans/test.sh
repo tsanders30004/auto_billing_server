4b909749e5669569ce0c8b3ba6890c42c4e4ccbf
Tim Sanders <tsanders@bluefin.com>
2020-04-28 09:06:49 -0400

    Added pentaho/trans/test.sh.

--- pentaho/trans/test.sh
+++ pentaho/trans/test.sh
@@ -1,19 +1,23 @@
 ########## Merchant ###################################################################################################
 # Output Table:  auto_billing_dw.d_merchant
 # Note:  Verify that we can always call auto_billing_dw.update_billing_frequency()
-cd /home/svc-dbwh/repositories/auto_billing/pentaho/trans/
-mysql -v -v -e "set foreign_key_checks=0; truncate auto_billing_dw.d_merchant; set foreign_key_checks=1;"
-/usr/local/install/data-integration/pan.sh -file dw_d_merchant.ktr
-mysql -v -v -e"call auto_billing_dw.update_billing_frequency()"
-read -p "press enter to run dw_f_decryptx_day.ktr..."
+#cd /home/svc-dbwh/repositories/auto_billing/pentaho/trans/
+#mysql -v -v -e "set foreign_key_checks=0; truncate auto_billing_dw.d_merchant; set foreign_key_checks=1;"
+#/usr/local/install/data-integration/pan.sh -file dw_d_merchant.ktr
+# mysql -v -v -e"call auto_billing_dw.update_billing_frequency()"
+# read -p "press enter to run dw_f_decryptx_day.ktr..."
 
 # ############################## f_decryptx_day #######################################################################
 # Output Tables:  auto_billing_dw.f_decryptx_day_alt and auto_billing_dw.f_decryptx_day
 #                 auto_billing_dw_d_device
-cd /home/svc-dbwh/repositories/auto_billing/pentaho/trans/
-mysql -v -e "set foreign_key_checks=0; truncate auto_billing_dw.f_decryptx_day_alt; truncate auto_billing_dw.f_decryptx_day; set foreign_key_checks=1;"
-/usr/local/install/data-integration/pan.sh -file dw_f_decryptx_day.ktr
-read -p "press enter to run dw_d_processor.ktr..."
+# cd /home/svc-dbwh/repositories/auto_billing/pentaho/trans/
+#mysql -v -e "set foreign_key_checks=0; truncate auto_billing_dw.f_decryptx_day_alt; truncate auto_billing_dw.f_decryptx_day; set foreign_key_checks=1;"
+#/usr/local/install/data-integration/pan.sh -file dw_f_decryptx_day.ktr
+
+# ############################## f_processor  #########################################################################
+# cd /home/svc-dbwh/repositories/auto_billing/pentaho/trans/
+/usr/local/install/data-integration/pan.sh -file dw_d_processor.ktr
+# read -p "press enter to dw_f_payconex_day.ktr..."
 
 # ############################## f_payconex_day #######################################################################
 # Output Table:  auto_billing_dw.f_payconex_day_alt and auto_billing_dw.f_payconex_day
